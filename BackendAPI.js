709f4ae10d13f1f80308b4fdb08f40c6e5b91122
Surya Sekar <suryasekar020@gmail.com>
2026-03-28 14:52:54 +0530

    Add Tally report with accurate stock calculation and item-wise grouping

--- BackendAPI.js
+++ BackendAPI.js
@@ -3271,30 +3271,31 @@ app.get('/report_dse_stock', (req, res) => {
             IFNULL(SUM(count), 0) AS count, 
             ROUND(IFNULL(SUM(silver), 0), 3) AS silver
         FROM (
-            SELECT s.dse, si.item, IFNULL(si.wt, 0) AS weight, IFNULL(si.count, 0) AS count, IFNULL(si.withcoverwt, 0) AS silver
+            SELECT s.dse, si.item, IFNULL(si.wt, 0) AS weight, IFNULL(si.count, 0) AS count, IFNULL(si.withcoverwt, 0) AS silver, 1 AS is_stock
             FROM stock s 
             JOIN stocksitem si ON s.stockid = si.stockid
             
             UNION ALL
             
-            SELECT sa.dse, sai.item, -IFNULL(sai.totalweight, 0) AS weight, -IFNULL(sai.count, 0) AS count, -IFNULL(sai.weight, 0) AS silver
+            SELECT sa.dse, sai.item, -IFNULL(sai.totalweight, 0) AS weight, -IFNULL(sai.count, 0) AS count, -IFNULL(sai.weight, 0) AS silver, 0 AS is_stock
             FROM sales sa 
             JOIN salesitem sai ON sa.invno = sai.invno
             
             UNION ALL
             
-            SELECT i.dse, ii.item, -IFNULL(ii.wt, 0) AS weight, -IFNULL(ii.count, 0) AS count, -IFNULL(ii.withcoverwt, 0) AS silver
+            SELECT i.dse, ii.item, -IFNULL(ii.wt, 0) AS weight, -IFNULL(ii.count, 0) AS count, -IFNULL(ii.withcoverwt, 0) AS silver, 0 AS is_stock
             FROM inventory i 
             JOIN inventoryitem ii ON i.inventid = ii.inventid
             
             UNION ALL
 
-            SELECT pm.dsename AS dse, pmi.item, -IFNULL(pmi.totalwt, 0) AS weight, -IFNULL(pmi.count, 0) AS count, -IFNULL(pmi.weight, 0) AS silver
+            SELECT pm.dsename AS dse, pmi.item, -IFNULL(pmi.totalwt, 0) AS weight, -IFNULL(pmi.count, 0) AS count, -IFNULL(pmi.weight, 0) AS silver, 0 AS is_stock
             FROM puremc pm
             JOIN puremcitem pmi ON pm.pureid = pmi.pureid
         ) t
         ${whereStr}
         GROUP BY dse, item
+        HAVING SUM(is_stock) > 0
         ORDER BY dse, item
     `;
 
